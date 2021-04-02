b94440b1c3de731a821d9e04e5aa815d4f666f72
Stephen Wright <wrightsah@icloud.com>
2021-04-02 01:22:04 -0400

    set up server listener

--- server.js
+++ server.js
@@ -2,5 +2,23 @@
 
 const mongojs = require('mongojs');
 
-// initialize database
+// require express
 
+const express = require('express');
+
+// initialize server on PORT 3000
+
+const app = express();
+const PORT = 3000;
+
+// db here
+
+
+// routes
+
+
+// server listener
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+})
