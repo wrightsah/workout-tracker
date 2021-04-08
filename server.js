321710b9b3e683d642a309640cc5c40b6aa056bd
Stephen Wright <wrightsah@icloud.com>
2021-04-08 15:05:40 -0400

    set static routes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // require mongojs
 
-const mongojs = require('mongojs');
+const mongojs = require("mongojs");
 
 // require express
 
-const express = require('express');
+const express = require("express");
 
-// require mongoose 
+// require mongoose
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // initialize server on PORT 3001
 
@@ -17,16 +17,17 @@ const PORT = process.env.PORT || 3001;
 
 // body parser middleware
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // file requirements - public, api, html
 
-
-
+app.use(express.static("public"));
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 // server listener
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
-})
+  console.log(`Listening on PORT ${PORT}`);
+});
